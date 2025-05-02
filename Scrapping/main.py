273dfe5fec4d0aca40408501f23a0584781c54f9
Ceorl <dev.rocher.florian@gmail.com>
2025-05-02 11:49:10 +0200

    finalisation de la fonction Scrap_one_element & création fonction UI (terminal)

--- Scrapping/main.py
+++ Scrapping/main.py
@@ -3,7 +3,8 @@ from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 
 
-def scrap_one_element(url):
+def scrap_one_element():
+    url = input("saississez votre url :\n")
     response = requests.get(url)
     soup = BeautifulSoup(response.text, "html.parser")
     book ={
@@ -15,13 +16,21 @@ def scrap_one_element(url):
         "number_available": soup.find("table").find("th", string="Availability").find_next_sibling("td").text, # id "content_inner" => table
         "product_description": soup.find("div", id="product_description").find_next_sibling("p").text, # id "product_description" élément suivant p
         "category": soup.find("ul", class_="breadcrumb").find_all("li")[2].find("a").text, # ul class "breadcrumb" 3eme li a
-        "review_rating": "", # p class "star-rating" /!\
+        "review_rating": soup.find("p", class_="star-rating")["class"][1], # p class "star-rating" /!\
         "image_url": urljoin("https://books.toscrape.com", soup.find("div", id="product_gallery").find("img")["src"]) # id "product_galery" => img src="" https://books.toscrape.com
     }
 
-    return book
+    return print(book)
 
 
-url = "https://books.toscrape.com/catalogue/crown-of-midnight-throne-of-glass-2_888/index.html"
+url = "https://books.toscrape.com/catalogue/the-secret-garden_413/index.html"
+def interface_user():
+    print("======================")
+    print("        Scrapping ")
+    print("======================")
+    print("1 - scrap 1 livre")
+    match input("Choississez votre fonction : "):
+        case "1": scrap_one_element()
+        case _: print("veuillez entrer une valeur valide")
 
-print(scrap_one_element(url))
+interface_user()
